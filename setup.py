55c097df1733e581e84e1e7b3a8515e8bc09bb06
April John <30842467+CutestNekoAqua@users.noreply.github.com>
2025-05-07 20:04:01 +0200

    i hate my life L0L

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+   name='automatic-ripping-machine',
+   version='@version@',
+   description='A automatic-ripping-machine',
+   license="MIT",
+   author='automatic-ripping-machine contributors',
+   packages=['automatic-ripping-machine'],  #same as name
+   install_requires=required, #external packages as dependencies
+)
